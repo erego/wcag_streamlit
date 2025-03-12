eb86764c6fe7235dacd908d33c4d572c27068f7d
erego <eladiorego@yahoo.es>
2025-03-12 12:42:29 +0100

    Mejora de la página de borrado para que desaparezca de la lista lo borrado

--- pages/1_gestion.py
+++ pages/1_gestion.py
@@ -16,28 +16,14 @@ col1, col2 = st.columns(2)
 path_raw= "./data/raw/"
 path_formatted= "./data/formatted/"
 
-with col1:
-    st.header("Ficheros en bruto")
-    for index, element in enumerate(os.listdir(path_raw)):
-        st.checkbox(path_raw + element, key='dynamic_checkbox_raw_' + str(index))
 
-with col2:
-    st.header("Ficheros saneados")
-    for index, element in enumerate(os.listdir(path_formatted)):
-        st.checkbox(path_formatted + element, key='dynamic_checkbox_formatted_' + str(index))
-
-
-
-delete_button = st.button("Borra los ficheros seleccionados")
-
-if delete_button:
+def form_callback():
     for index, element in enumerate(os.listdir(path_raw)):
         name = 'dynamic_checkbox_raw_' + str(index)
 
         if st.session_state[name] is True:
             os.unlink(path_raw + element)
-            st.write( f"Borrado fichero {path_raw + element }")
-            
+            st.write( f"Borrado fichero {path_raw + element }")          
 
 
     for index, element in enumerate(os.listdir(path_formatted)):
@@ -45,4 +31,22 @@ if delete_button:
 
         if st.session_state[name] is True:
             os.unlink(path_formatted + element)
-            st.write( f"Borrado fichero {path_formatted + element }")
+            st.write( f"Borrado fichero {path_formatted + element }")  
+
+with st.form("form_delete"):
+    
+    with col1:
+        st.header("Ficheros en bruto")
+        for index, element in enumerate(os.listdir(path_raw)):
+            st.checkbox(path_raw + element, key='dynamic_checkbox_raw_' + str(index))
+
+    with col2:
+        st.header("Ficheros saneados")
+        for index, element in enumerate(os.listdir(path_formatted)):
+            st.checkbox(path_formatted + element, key='dynamic_checkbox_formatted_' + str(index))
+
+
+
+    delete_button = st.form_submit_button("Borra los ficheros seleccionados", on_click=form_callback)
+
+    
