c41590e94d8c4a8524470fc9079028a1a05bc91a
erego <eladiorego@yahoo.es>
2025-04-13 09:26:18 +0200

    Mejora de las funciones del módulo de wcag para comprobar compatibilidades y obtener la mejor versi´n de wcag y añadido test de las mismas

--- data_api/wcag_operations.py
+++ data_api/wcag_operations.py
@@ -17,6 +17,74 @@ def get_config_toml_wcag():
     return versions_wcag
 
 
+def is_compatible_wcag_version(path_to_file:str, wcag_version:str)->bool : 
+    """Función que comprueba si determinada versión de wcag es compatible con un fichero. Devuelve falso si no lo es
+
+    Args:
+        path_to_file (str): _description_
+        wcag_version (str): _description_
+    Returns:
+        bool: Devuelve True si lo es y False en caso contrario
+    """
+
+    config_versions_wcag= get_config_toml_wcag()
+
+
+    for version_wcag in config_versions_wcag:
+
+        if version_wcag['version'] == wcag_version:
+            version_config_to_test = version_wcag
+            
+            break
+
+    data_wcag = pd.read_excel(path_to_file, index_col = 0)
+    data_wcag_subtable = data_wcag.loc[:,["Sucess_Criterion", "Principles_Guidelines"]] 
+    data_wcag_subtable = data_wcag_subtable.dropna()
+    data_wcag_subtable = data_wcag_subtable["Principles_Guidelines"]
+    data_wcag_subtable = data_wcag_subtable.reset_index(drop=True)
+    data_wcag_criterions = data_wcag_subtable.tolist()
+    
+    is_compatible = True
+    # Recorremos los criterios segun la configuración de esa versión a comprobar
+    criterions_to_check = version_config_to_test['success_criterion']
+
+    for criterion_to_check in criterions_to_check:
+
+        found_criterion = False
+
+        # Buscamos el criterio en la tabla o alguno similar
+        for value in data_wcag_criterions:
+
+            # Tienen que pertenecer al mismo guideline
+            if value[0:3] != criterion_to_check[0:3]:
+                continue
+
+
+            excel_criterion = value.strip()
+
+            if criterion_to_check != excel_criterion:
+
+                if excel_criterion not in criterion_to_check:
+
+                    similitud = SM(None, criterion_to_check, value.strip()).ratio()
+                    
+                    if similitud >= 0.75:
+                        found_criterion = True  
+                        break
+                else:
+                    found_criterion = True
+                    break
+
+            else:
+                found_criterion = True
+                break
+
+        if found_criterion == False:
+            is_compatible = False 
+            break     
+
+    return is_compatible
+
 def get_best_wcag_compability(path_to_file:str)-> str:
     """Función que devuelve la mejor compatibilidad de un fichero con una versión de wcag
 
@@ -26,48 +94,17 @@ def get_best_wcag_compability(path_to_file:str)-> str:
     Returns:
         str: Devuelve la mejor version para la que es compatible
     """
-    data_wcag = pd.read_excel(path_to_file, index_col = 0)
-
-    found_compatible_criterions = True
-    version_compatible = None
-    data_wcag_subtable = data_wcag.loc[:,["Sucess_Criterion", "Principles_Guidelines"]] 
-    data_wcag_subtable = data_wcag_subtable.dropna()
-    data_wcag_subtable = data_wcag_subtable["Principles_Guidelines"]
-    data_wcag_subtable = data_wcag_subtable.reset_index(drop=True)
-    num_criterions_table = data_wcag_subtable.shape[0]
-
+ 
     config_versions_wcag= get_config_toml_wcag()
 
     for config_version_wcag in reversed(config_versions_wcag):
 
-        
-        criterions_to_check = config_version_wcag['success_criterion']
+        version_to_check = config_version_wcag["version"]
 
-        num_criterions_config = len(criterions_to_check)
-        if num_criterions_table != num_criterions_config:
-            continue
-        else:
-  
-            for index, value in data_wcag_subtable.items():
-                # Get element index from list of criterion
-                criterion_to_check = criterions_to_check[index].strip()
-                excel_criterion = value.strip()
-
-                if criterion_to_check != excel_criterion:
-
-                    if excel_criterion not in criterion_to_check:
-
-                        similitud = SM(None, criterion_to_check, value.strip()).ratio()
-                        
-                        if similitud < 0.75:
-                            found_compatible_criterions = False         
-        
-
-            if found_compatible_criterions is True:
-                version_compatible = config_version_wcag["version"]
-                break
-
-    return version_compatible
+        if is_compatible_wcag_version(path_to_file, version_to_check):
+            return version_to_check
+ 
+    return None
 
 
 def get_wcag_compability(path_to_file:str, version_to_compare:dict)-> bool:
--- tests/test_wcag.py
+++ tests/test_wcag.py
@@ -1,13 +1,36 @@
 import pandas as pd
 from difflib import SequenceMatcher as SM
-from wcag.data_api.wcag_operations import get_config_toml_wcag, get_best_wcag_compability
+from wcag.data_api.wcag_operations import get_config_toml_wcag, get_best_wcag_compability, is_compatible_wcag_version
+import pytest
+
 
 
 def test_check_version_compatible():
 
+    version_to_test = "2.1"
+
+    result = is_compatible_wcag_version('./tests/WCAG_ayuntamientos_formatted.xlsx', version_to_test)
+
+    assert result is True
+
+    version_to_test = "2.0"
+
+    result = is_compatible_wcag_version('./tests/WCAG_ayuntamientos_formatted.xlsx', version_to_test)
+
+    assert result is True
+
+    version_to_test = "2.2"
+
+    result = is_compatible_wcag_version('./tests/WCAG_ayuntamientos_formatted.xlsx', version_to_test)
+
+    assert result is False
+
+
+def test_check_best_version_compatible():
+
     version_compatible = get_best_wcag_compability('./tests/WCAG_ayuntamientos_formatted.xlsx')
     
-    assert version_compatible == '2.2'
+    assert version_compatible == '2.1'
 
 
 def test_wcag_adjust():
@@ -26,44 +49,58 @@ def test_wcag_adjust():
             
             break
 
-    assert version_to_test is not None
+    compatibility = is_compatible_wcag_version('./tests/WCAG_ayuntamientos_formatted.xlsx', version_to_test["version"])
+
+    assert compatibility is True
 
     criterions_to_check = version_to_test['success_criterion']
-
-    found_criterions = True
 
     data_wcag_subtable = data_wcag.loc[:,["Sucess_Criterion", "Principles_Guidelines"]] 
     data_wcag_subtable = data_wcag_subtable.dropna()
     data_wcag_subtable = data_wcag_subtable["Principles_Guidelines"]
     data_wcag_subtable = data_wcag_subtable.reset_index(drop=True)
 
-    num_criterions_table = data_wcag_subtable.shape[0]
-    num_criterions_config = len(criterions_to_check)
+    data_wcag_criterions = data_wcag_subtable.tolist()
+    
+    num_criterions_losts = 0
+    for criterion_to_check in criterions_to_check:
+     
+        found_criterion = False
+        value_found = False
+        # Buscamos el criterio en la tabla o alguno similar
+        for value in data_wcag_criterions:
 
-    assert num_criterions_table == num_criterions_config
+            # Tienen que pertenecer al mismo guideline
+            if value[0:3] != criterion_to_check[0:3]:
+                continue
 
-    num_error = 0
+            excel_criterion = value.strip()
 
-    for index, value in data_wcag_subtable.items():
-        # Get element index from list of criterion
-        criterion_to_check = criterions_to_check[index].strip()
-        excel_criterion = value.strip()
+            if criterion_to_check != excel_criterion:
 
-        if criterion_to_check != excel_criterion:
-
-            if excel_criterion in criterion_to_check:
-                data_wcag.loc[data_wcag['Principles_Guidelines'] == value, 'Principles_Guidelines'] = criterion_to_check
-
-            else:
-                similitud = SM(None, criterion_to_check, value.strip()).ratio()
-                
-                if similitud < 0.75:
-                    found_criterions = False         
-                    error+= 1
-                else:
+                if excel_criterion in criterion_to_check:
                     data_wcag.loc[data_wcag['Principles_Guidelines'] == value, 'Principles_Guidelines'] = criterion_to_check
+                    found_criterion = True
+                    value_found = value
+                    break
+                else:
+                    similitud = SM(None, criterion_to_check, value.strip()).ratio()
+                    
+                    if similitud >= 0.75:
+                        data_wcag.loc[data_wcag['Principles_Guidelines'] == value, 'Principles_Guidelines'] = criterion_to_check
+                        found_criterion = True
+                        value_found = value
+                        break
+            else:
+                found_criterion = True
+                value_found = value
 
-  
-    assert num_error == 0
-    assert found_criterions is True
 
+        if found_criterion is False:
+            num_criterions_losts+=1
+        else:
+            data_wcag_criterions.remove(value_found)
+
+    assert num_criterions_losts == 0  
+    assert len(data_wcag_criterions) == 0                    
+    data_wcag.to_excel('./tests/WCAG_ayuntamientos_formatted_check.xlsx')
