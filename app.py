b10c33781c93aaf1edd54dd17f2550090fa937de
erego <eladiorego@yahoo.es>
2025-05-03 18:46:33 +0200

    Añadido un chatbot al sistema

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ pagina_inicio = st.Page("./web_pages/init.py", title= "Inicio")
 pagina_gestion = st.Page("./web_pages/gestion.py", title= "Gestión de ficheros")
 pagina_visualizacion = st.Page("./web_pages/visualizacion.py", title= "Visualización de datos")
 pagina_calidad = st.Page("./web_pages/calidad.py", title= "Calidad de datos")
+pagina_chatbot = st.Page("./web_pages/chatbot.py", title= "Chatbot de consulta")
 
 
 
 
-pg = st.navigation([pagina_inicio, pagina_gestion, pagina_visualizacion, pagina_calidad])
+pg = st.navigation([pagina_inicio, pagina_gestion, pagina_visualizacion, pagina_calidad, pagina_chatbot])
 pg.run()
